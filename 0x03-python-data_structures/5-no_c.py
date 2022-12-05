16e42e0918b7bb47a59dc13c2337b73f1a298c34
Teddystone08 <ernestlrty@hotmail.com>
2022-12-05 17:38:45 +0000

    copy list

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string_list = list(my_string)
+    index_count = 0
+    for i in my_stirng_list:
+        for i == 'c' or i == 'C':
+            my_stringlist[index_count] = ""
+        index_count += 1
+    return "".join(my_string_list)
