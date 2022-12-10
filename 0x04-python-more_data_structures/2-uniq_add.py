7ab3f765f69f4054c8d9626c965dd8182802bb01
Teddystone08 <ernestlrty@hotmail.com>
2022-12-10 09:41:01 +0000

    More Data Structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum = 0
-    unqui_int = set(my_list)
-    for number in uniqi_int:
-        if number is unqui_int:
-         unqui_int.sum(unqui_int)
-        return sum
+    for number in my_list:
+        sum += number
+    return sum
