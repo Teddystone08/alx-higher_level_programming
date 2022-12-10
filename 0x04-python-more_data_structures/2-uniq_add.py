4fac35c95bcd88ac615f9ad9b12381813a9c7418
Teddystone08 <ernestlrty@hotmail.com>
2022-12-10 04:44:12 +0000

    unque add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,6 @@
 def uniq_add(my_list=[]):
     sum = 0
     unqui_int = set(my_list=[])
-    for number in uniq_int:
+    for number in uniqi_int:
         sum[unqui_int] = sum.get(unqui_int, 0) + 1
         return sum
