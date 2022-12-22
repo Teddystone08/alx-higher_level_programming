6488f5f0ef5ee5d27c1620f70183db1b744ce59b
Teddystone <ernestlrty@hotmail.com>
2022-12-22 17:06:05 +0000

    integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/bin/python3
+
 def safe_print_integer(value):
+    """ pirnt integers with {:d}.format()."""
+
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except:(typoerror, valueerror):
         return False
