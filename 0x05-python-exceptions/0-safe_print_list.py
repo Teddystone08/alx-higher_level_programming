e23acf832a83fb86dd0b00dbccc15f6b673a8d35
Teddystone <ernestlrty@hotmail.com>
2022-12-22 16:25:56 +0000

    integers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     i = 0
     printed = 0
@@ -7,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             printed += 1
-        except:
+        except: indexerror:
             continue
     print()
     return printed
