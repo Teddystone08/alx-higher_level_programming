12094f77a0a38e24dbba01c940d9c05139570042
Teddystone <ernestlrty@hotmail.com>
2022-12-31 18:27:17 +0000

    numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for number in range(99):
-    print('{:02d}'.format(number), end=',')
-    print(number)
+for count in range(99):
+    print('{:02d}'.format(count), end=', ')
+print('99')
