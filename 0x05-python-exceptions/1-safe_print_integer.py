62693722d3171d25089d72bd004bd895eee95e46
Teddystone08 <ernestlrty@hotmail.com>
2023-01-12 21:47:40 +0000

    integer list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,12 @@
 #!/bin/python3
 
 def safe_print_integer(value):
-    """ pirnt integers with {:d}.format()."""
+    """ pirnt integers with {:d}.format().
+    value an integer or string of input
 
+    """
     try:
         print("{:d}".format(value))
         return True
-    except:(typoerror, valueerror):
+    except valueerror:
         return False
