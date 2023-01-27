2e68655b8e896e50d72a0c77deb76b3b04b591cd
Teddystone08 <ernestlrty@hotmail.com>
2023-01-27 16:55:42 +0000

    empty class

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
-
+"""module with class BaseGeometry to be called"""
 
 class BaseGeometry:
     """empty class"""
-
