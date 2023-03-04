984bad999624138f6322dead7d806da2c94f2960
Teddystone08 <ernestlrty@hotmail.com>
2023-03-04 14:25:23 +0000

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module square size
+"""
+
+
+def __init__(self, size=0):
+    """Initialises the data"""
+    self.__size == size
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
