a9e3c26546f31e012389474329767c2a47cc1bdf
Teddystone08 <ernestlrty@hotmail.com>
2023-03-06 17:13:54 +0000

    python i/0

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 
+
 import json
+
 
 def to_json_string(my_obj):
     """
     Converts an object to a JSON string representation.
-    
+
     Args:
         my_obj: The object to be converted to JSON.
-        
+
     Returns:
         A string that represents the JSON serialization of the object.
-        
+
     Raises:
-        Any exception raised by the `json.dumps()` method, if the object cannot be serialized to JSON.
+        Any exception raised by the `json.dumps()` methdo,
+        if the object cannot be serialized to JSON.
     """
     return json.dumps(my_obj)
-
