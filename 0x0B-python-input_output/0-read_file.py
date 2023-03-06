ed3fcfdbe0d7d7cc46d7deb54d9ea67cd344aec3
Teddystone08 <ernestlrty@hotmail.com>
2023-03-06 16:00:46 +0000

    python i/0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """function read file"""
 
+
 def read_file(filename=""):
     with open(filename, "f", encoding="UTF-8") as f:
         for line in file:
-        print(line, end="")
+            print(line, end="")
