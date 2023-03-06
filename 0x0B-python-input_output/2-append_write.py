ba0e4670f33087603a959647412f3445853facde
Teddystone08 <ernestlrty@hotmail.com>
2023-03-06 16:45:50 +0000

    python i/0

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""module function append"""
+
+
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding="UTF-8") as f:
+    """function append"""
+
+    with open(filename, "a", encoding="UTF-8") as file:
+
         return f.write(text)
-    
