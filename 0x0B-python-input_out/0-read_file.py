8ffdd5dcd41741776f300fd009f09f01a12478e9
Teddystone08 <ernestlrty@hotmail.com>
2023-03-06 15:28:49 +0000

    read file

--- 0x0B-python-input_out/0-read_file.py
+++ 0x0B-python-input_out/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def red_file(filename=""):
+"""function read file"""
+
+def read_file(filename=""):
     with open(filename, "f", encoding="UTF-8") as f:
         print(f.read(), end="")
