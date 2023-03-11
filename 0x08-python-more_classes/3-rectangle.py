78933303d688a21879087e253ad80b99311d744e
Teddystone08 <ernestlrty@hotmail.com>
2023-03-11 09:24:55 +0000

    s representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""Module of a rectangle"""
+
+
+class Rectangle:
+    """empty class method for rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize instances with attributes"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the value of __width
+        Sets the value of __width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the value of __height
+        Sets the value of __height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Rectangle area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Rectangle perimeter
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Creates a new string object from the given object
+        """
+        s = ''
+        if self.__width > 0 and self.__height > 0:
+            for _ in range(self.__height):
+                for _ in range(self.__width):
+                    s += '#'
+                s += '\n'
+        return s[: -1]
