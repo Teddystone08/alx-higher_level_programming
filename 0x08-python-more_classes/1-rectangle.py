a13f09b5806489f54952d3d8328555c7011933e7
Teddystone08 <ernestlrty@hotmail.com>
2023-03-11 08:18:47 +0000

    definition

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,22 +6,21 @@
 class Rectangle:
     """empty class method for rectangle"""
 
-    def width(self, width=0, height=0):
-        """initialising"""
+    def __init__(self, width=0, height=0):
+        """Initialize instances with attributes"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Getter method"""
-
+        """Get the value of __width
+        Sets the value of __width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """"Setter method"""
-
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -29,15 +28,14 @@ class Rectangle:
 
     @property
     def height(self):
-        """Getter method"""
-
+        """Get the value of __height
+        Sets the value of __height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter method"""
-
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
