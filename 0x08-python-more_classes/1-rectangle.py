d61a45976de364b7034e6b0ab692009218a14481
Teddystone08 <ernestlrty@hotmail.com>
2023-03-11 08:09:16 +0000

    definition

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Module of a rectangle"""
+
+
+class Rectangle:
+    """empty class method for rectangle"""
+
+    def width(self, width=0, height=0):
+        """initialising"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter method"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """"Setter method"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method"""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
