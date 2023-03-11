7b7cd89b2c28bc63cefc6a29217f7ae2e07b134e
Teddystone08 <ernestlrty@hotmail.com>
2023-03-11 08:51:40 +0000

    area & Pmeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,7 +42,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Rectangle area"""
+        return self.__width * self.__height
+
+    def primeter(self):
         """define the perimeter of a rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * self.__width + 2 * self.__height
+        return 2 * (self.__width + self.__height)
