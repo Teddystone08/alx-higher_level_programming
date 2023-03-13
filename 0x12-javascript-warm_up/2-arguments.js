cdc661595c2ec8eacadbbc2de00f093c35fc05a1
Teddystone08 <ernestlrty@hotmail.com>
2023-03-13 16:49:34 +0000

    Argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
-#!/usr/bin/node
-const mic = process.argv;
-if (mic.lenght < 1) {
-	console.log('No argument');
-}else if (mic.lenght === 1){
-	console.log('Argument found');
-}else {
-	console.log('Argument found');
+#!/usr/bin/python
+
+const len = process.argv;
+if (len.length === 0) {
+  console.log('No argument');
+} else if (len.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
