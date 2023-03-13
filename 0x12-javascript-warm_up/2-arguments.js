8ae946c4a71292a52fdd8763bd69f8415a3a1fb0
Teddystone08 <ernestlrty@hotmail.com>
2023-03-13 16:23:31 +0000

    Argument

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const mic = process.argv;
+if(mic < 1){
+	console.log('No argument');
+}else if(mic === 1){
+	console.log('Argument found');
+}else{
+	console.log('Argument found');
