abc2a049443180688a65997b3600d53997dfca65
Teddystone08 <ernestlrty@hotmail.com>
2023-03-14 14:09:56 +0000

    create

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const go = process.argv[2];
-const sit = process.argv[3];
-console.log('${go} is ${sit}');
+const a1 = process.argv[2];
+const a2 = process.argv[3];
+console.log('${a1`} is ${a2}');
