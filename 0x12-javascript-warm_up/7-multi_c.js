34a12f786b3e693cf9737eaa0ad7926e4b20797c
Teddystone08 <ernestlrty@hotmail.com>
2023-03-14 15:36:17 +0000

    love c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (!process.argv[2]) {
+  console.log('Missing number of occurences');
+} else if (process.argv[2] > 0) {
+  for (let i = process.argv[2]; i; --i) {
+    console.log('C is fun');
+  }
+}
