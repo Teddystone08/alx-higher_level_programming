5db54c8fbeaaf11790fbab75e70566cf674dc64d
Teddystone08 <ernestlrty@hotmail.com>
2023-03-14 17:24:27 +0000

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < myVar; i++) {
   console.log(myVar[i]);
 }
