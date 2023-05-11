94b8f95e080058584c3fc6449d595bca896ea12a
Teddystonecold08 <ernestlrty@hotmail.com>
2023-05-11 10:41:40 +0000

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const filePath = process.argv[2];
 const fs = require('fs');
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
+  } else {
+    console.log(data);
   }
-  console.logo(data);
 });
