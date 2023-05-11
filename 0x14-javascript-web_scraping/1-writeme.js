3d9c5f8c13783a327a71feb5f723306d818f75d7
Teddystonecold08 <ernestlrty@hotmail.com>
2023-05-11 10:48:45 +0000

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (error) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  console.log('File saved successfully.');
+fs.appendFile(process.argv[2], process.argv[3], 'utf8', function (error) {
+  if (error) throw error;
 });
