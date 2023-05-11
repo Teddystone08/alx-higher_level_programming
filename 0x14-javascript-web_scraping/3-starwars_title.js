3388cfb12ece6b59607413fc787093a03692d78b
Teddystonecold08 <ernestlrty@hotmail.com>
2023-05-11 11:03:27 +0000

    js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const req = require('request');
 req.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/', function (error, response, body) {
   if (error) throw error;
