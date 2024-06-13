ab9aa8a30880df054c8d712667f4caf6d7cf9a4e
Teddystone08 <ernestlrty@hotmail.com>
2024-06-13 21:08:43 +0000

    dumps

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+"""
+to JSON string
 
+"""
 
 import json
 
 
 def to_json_string(my_obj):
     """
-    Converts an object to a JSON string representation.
-
-    Args:
-        my_obj: The object to be converted to JSON.
-
-    Returns:
-        A string that represents the JSON serialization of the object.
+    return JSON format of my_obj
     """
+
     return json.dumps(my_obj)
