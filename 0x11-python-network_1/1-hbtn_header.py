21608d7084f5412fd7f0d769be8d179742c0f51f
Teddystone08 <ernestlrty@hotmail.com>
2024-07-08 12:46:01 +0000

    i/o

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,11 +7,7 @@ script that receive and send request
 if __name__ == '__main__':
     import sys
     import urllib.request
-
     url = sys.argv[1]
-
     with urllib.request.urlopen(url) as response:
-        url_head = response.getheaders()
-        for url_head in headers
-        if url_head[0] == 'X-Request-Id':
-            print(url_head[1])
+        url_head = response.info()
+        print(url_head['X-Request-Id'])
