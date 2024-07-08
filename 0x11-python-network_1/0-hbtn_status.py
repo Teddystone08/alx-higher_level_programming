bcb55deec49ce22c399e793f5a343ab9582acf9e
Teddystone08 <ernestlrty@hotmail.com>
2024-07-08 09:48:28 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,4 +2,8 @@
 import urllib.request
 
 with urllibe.request.urlopen(https: // alx-intranet.hbtn.io/status)
-as response: html = response.read()
+as response:
+    content = response.read().decode('utf-8')
+    print("Body response:")
+    print(f"\t- type: {content-type}")
+    print(f"\t- content: {conten}")
