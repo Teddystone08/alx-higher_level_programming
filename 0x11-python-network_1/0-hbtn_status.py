ccc5e7cf0ec3646d7cf5bdb17221315027329701
Teddystone08 <ernestlrty@hotmail.com>
2024-07-08 10:48:48 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,13 @@
 script to fetch url
 """
 
+
 if __name__ == '__main__':
     import urllib.request
-    with urllibe.request.urlopen(https: // alx-intranet.hbtn.io/status)
-    as response:
-        content = response.read().decode('utf-8')
-        print("Body response:")
-        print(f"\t- type: {content-type}")
-        print(f"\t- content: {conten}")
+    url = 'alx-intranet.hbtn.io/status'
+    with urllibe.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print('Body response:')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode))
