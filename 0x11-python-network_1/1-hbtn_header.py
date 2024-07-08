cccd004965f4d5f78ff24c0defc8b32a4a0ae364
Teddystone08 <ernestlrty@hotmail.com>
2024-07-08 12:39:29 +0000

    i/o

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,5 +11,7 @@ if __name__ == '__main__':
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        url_head = response.info()
-        print(url_head['X-Request-Id'])
+        url_head = response.getheaders()
+        for url_head in headers
+        if url_head[0] == 'X-Request-Id':
+            print(url_head[1])
