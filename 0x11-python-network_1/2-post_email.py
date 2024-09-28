6d78b84893573fd83b88e465f3e67557db033c34
Teddystone08 <ernestlrty@hotmail.com>
2024-09-28 15:47:53 +0000

    delete

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that receive and send request
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        url_head = response.info()
+        print(url_head['X-Request-Id'])
